d72b457b8b6367c1b358db33b5301d073a662efc
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-05-07 06:41:51 +0600

    static auto complete add

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+// import ComboBox from "./components/ComboBox";
+import Home from "./components/Home";
+
+function App() {
+  return (
+    <>
+        <div
+          style={{
+            display: "block",
+            justifyContent: "center",
+            alignItems: "center",
+            margin: "10"
+          }}
+        >
+          
+          <Home/>
+          
+        </div>
+    </>
+  );
+}
+
+
+export default App;
