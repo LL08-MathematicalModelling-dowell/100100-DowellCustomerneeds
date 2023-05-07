63062a62e3c689e2b2b4d2f5ec08aa3f75b85948
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-05-07 06:48:44 +0600

    initial

--- connect_api_backend/connect/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ConnectDataView, ConnectDataTwoView, ConnectDataThreeView
-
-urlpatterns = [
-    path('api/spreadsheetOne/', ConnectDataView.as_view()),
-    path('api/spreadsheetTwo/', ConnectDataTwoView.as_view()),
-    path('api/spreadsheetThree/', ConnectDataThreeView.as_view()),
-]
