581217d38aec1cc800e37a4c1ab693eb1629c196
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-05-11 09:55:58 +0600

    autocomplitor error fixed

--- frontend/src/components/AutoCompleter.jsx
+++ frontend/src/components/AutoCompleter.jsx
@@ -1,86 +1,134 @@
 import * as React from "react";
 import Card from "@mui/material/Card";
-import axios from "axios";
 import { useState, useEffect } from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import Box from "@mui/material/Box";
+import { Button, CircularProgress } from "@mui/material";
 
 export default function AutoCompleter() {
-  
-  const baseURL = "http://127.0.0.1:8000/api/services";
-  const [rows, setRows] = useState([]);
-  const [rowdata, setRowdata] = useState([]);
+  const [q1Data, setQ1Data] = useState([]);
+  const [q2Data, setQ2Data] = useState([]);
+  const [q3Data, setQ3Data] = useState([]);
+  const [selectedQ1Data, setSelectedQ1Data] = useState(null);
+  const [selectedQ2Data, setSelectedQ2Data] = useState(null);
+  const [selectedQ3Data, setSelectedQ3Data] = useState(null);
+
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    axios.get(baseURL).then((response) => {
-      setRows(response.data);
-    });
+    fetch("http://127.0.0.1:8000/q1/")
+      .then((response) => response.json())
+      .then((data) => {
+        setQ1Data(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching q1 data:", error);
+      });
+
+    fetch("http://127.0.0.1:8000/q2/")
+      .then((response) => response.json())
+      .then((data) => {
+        setQ2Data(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching q2 data:", error);
+      });
+
+    fetch("http://127.0.0.1:8000/q3/")
+      .then((response) => response.json())
+      .then((data) => {
+        setQ3Data(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching q3 data:", error);
+      });
   }, []);
 
-  
-  useEffect(() => {
-    if (rowdata) {
-      setRows([rowdata]);
-    } else {
-      axios.get(baseURL).then((response) => {
-        setRows(response.data);
-        console.log(response.data);
-      });
-    }
-  }, [rowdata]);
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
 
+    const payload = {
+      q1: selectedQ1Data,
+      q2: selectedQ2Data,
+      q3: selectedQ3Data,
+    };
+
+    fetch("http://127.0.0.1:8000/api/services/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(payload),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Data posted successfully:", data);
+        setLoading(false);
+        alert("Data saved successfully!");
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.error("Error posting data:", error);
+      });
+  };
 
   return (
     <>
-      {rows ? (
-        <Card sx={{ minWidth: 1200, m: 8 }} elevation={11}>
-          <Box
-            component="span"
-            m={1}
-            pr={1}
-            p={4}
-            display="flex"
-            justifyContent="center"
+      <Card sx={{ minWidth: 1200, m: 8 }} elevation={11}>
+        <Box
+          component="form"
+          onSubmit={handleFormSubmit}
+          m={1}
+          pr={1}
+          p={4}
+          display="flex"
+          justifyContent="center"
+        >
+          <Autocomplete
+            disablePortal
+            id="q1"
+            options={q1Data}
+            value={selectedQ1Data}
+            onChange={(e, v) => setSelectedQ1Data(v)}
+            sx={{ width: 400 }}
+            getOptionLabel={(q1Data) => q1Data.Item || ""}
+            renderInput={(params) => (
+              <TextField {...params} label="I am from" size="small" />
+            )}
+          />
+          <Autocomplete
+            disablePortal
+            id="q2"
+            options={q2Data}
+            value={selectedQ2Data}
+            onChange={(e, v) => setSelectedQ2Data(v)}
+            sx={{ width: 400 }}
+            getOptionLabel={(q2Data) => q2Data.Item || ""}
+            renderInput={(params) => (
+              <TextField {...params} label="How to" size="small" />
+            )}
+          />
+          <Autocomplete
+            disablePortal
+            id="q3"
+            options={q3Data}
+            value={selectedQ3Data}
+            onChange={(e, v) => setSelectedQ3Data(v)}
+            sx={{ width: 400 }}
+            getOptionLabel={(q3Data) => q3Data.Item || ""}
+            renderInput={(params) => (
+              <TextField {...params} label="For" size="small" />
+            )}
+          />
+
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleFormSubmit}
           >
-            <Autocomplete
-              disablePortal
-              id="combo-box-demo"
-              options={rows}
-              onChange={(e, v) => setRowdata(v)}
-              sx={{ width: 400 }}
-              getOptionLabel={(rows) => rows.category || ""}
-              renderInput={(params) => (
-                <TextField {...params} label="Search Category" size="small" />
-              )}
-            />
-            <Autocomplete
-              disablePortal
-              id="combo-box-demo"
-              options={rows}
-              onChange={(e, v) => setRowdata(v)}
-              sx={{ width: 400 }}
-              getOptionLabel={(rows) => rows.location || ""}
-              renderInput={(params) => (
-                <TextField {...params} label="Search Location" size="small" />
-              )}
-            />
-            <Autocomplete
-              disablePortal
-              id="combo-box-demo"
-              options={rows}
-              onChange={(e, v) => setRowdata(v)}
-              sx={{ width: 400 }}
-              getOptionLabel={(rows) => rows.product || ""}
-              renderInput={(params) => (
-                <TextField {...params} label="Search product" size="small" />
-              )}
-            />
-          </Box>
-        </Card>
-      ) : (
-        <h3>Loading...</h3>
-      )}
+            {loading ? <CircularProgress size={24} /> : "Submit"}
+          </Button>
+        </Box>
+      </Card>
     </>
   );
 }
