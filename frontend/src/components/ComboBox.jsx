68ba55f043bc433fca943f155b2b7059a3d94558
taufique01 <taufiquehr@gmail.com>
2023-07-14 18:56:31 +0600

    WIP: refactoring question selection

--- frontend/src/components/ComboBox.jsx
+++ frontend/src/components/ComboBox.jsx
@@ -1,32 +1,34 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import { AllBasket } from './permutation/AllBasket';
-import DataCalculation from './DataCalculation';
-
-
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import { AllBasket } from "./permutation/AllBasket";
+import DataCalculation from "./DataCalculation";
+import { QuestionsSelection } from "./QuestionsSelection";
 
 const ComboBox = () => {
+  const [selectedOptions, setSelectedOptions] = React.useState();
   return (
     <Container sx={{ minWidth: 250, maxWidth: 1200, m: 8 }}>
-    <Box display="flex"
-    flexDirection={{ xs: "column", md: "row"}}>
-      <Grid container spacing={2}  alignItems="center"
-      justifyContent="center">
-        <Grid item xs={12} sm={6} >
-          
-            <AllBasket/>
-          
-        </Grid>
-        <Grid item xs={12} sm={6}>          
-            <DataCalculation/>
-         
-        </Grid>
-      </Grid>
-    </Box>
-    </Container>
-  )
-}
+      <Box display="flex" flexDirection={{ xs: "column", md: "row" }}>
+        <Grid container spacing={2} alignItems="center" justifyContent="center">
+          <Box>
+            <QuestionsSelection
+              onSelectionChange={(value) => setSelectedOptions(value)}
+            />
+          </Box>
 
-export default ComboBox
+          <Grid item xs={12} sm={6}>
+            <AllBasket selectedOptions={selectedOptions} />
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <DataCalculation selectedOptions={selectedOptions} />
+          </Grid>
+        </Grid>
+      </Box>
+    </Container>
+  );
+};
+
+export default ComboBox;
--- frontend/src/components/DataCalculation.jsx
+++ frontend/src/components/DataCalculation.jsx
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import LoadingButton from "@mui/lab/LoadingButton";
 import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
 import Box from "@mui/material/Box";
 import { Button, CircularProgress } from "@mui/material";
-// table
+
 import {
   TableContainer,
   Table,
@@ -36,58 +34,16 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-export default function DataCalculation() {
-  const [q1Data, setQ1Data] = useState([]);
-  const [q2Data, setQ2Data] = useState([]);
-  const [q3Data, setQ3Data] = useState([]);
-  const [spreadData, setSpreadData] = useState([]);
-  const [selectedQ1Data, setSelectedQ1Data] = useState(null);
-  const [selectedQ2Data, setSelectedQ2Data] = useState(null);
-  const [selectedQ3Data, setSelectedQ3Data] = useState(null);
-
-  const [loading, setLoading] = useState(false);
+export default function DataCalculation({
+  selectedQ1Data,
+  selectedQ2Data,
+  selectedQ3Data,
+}) {
   const [loadingSpread, setLoadingSpread] = useState(false);
+
   const isSubmitDisabled =
     !selectedQ1Data || !selectedQ2Data || !selectedQ3Data;
-
-  useEffect(() => {
-    setLoading(true); // set loading to true when the fetch request is initiated
-    setTimeout(() => {
-      // add a 2s delay before fetching the data
-      fetch("http://127.0.0.1:8000/q1/")
-        .then((response) => response.json())
-        .then((data) => {
-          setQ1Data(data);
-          setLoading(false); // set loading to false when the data is fetched
-        })
-        .catch((error) => {
-          console.error("Error fetching q1 data:", error);
-          setLoading(false); // set loading to false when an error occurs
-        });
-
-      fetch("http://127.0.0.1:8000/q2/")
-        .then((response) => response.json())
-        .then((data) => {
-          setQ2Data(data);
-          setLoading(false); // set loading to false when the data is fetched
-        })
-        .catch((error) => {
-          console.error("Error fetching q2 data:", error);
-          setLoading(false); // set loading to false when an error occurs
-        });
-
-      fetch("http://127.0.0.1:8000/q3/")
-        .then((response) => response.json())
-        .then((data) => {
-          setQ3Data(data);
-          setLoading(false); // set loading to false when the data is fetched
-        })
-        .catch((error) => {
-          console.error("Error fetching q3 data:", error);
-          setLoading(false); // set loading to false when an error occurs
-        });
-    }, 2000);
-  }, []);
+  const [spreadData, setSpreadData] = useState([]);
 
   const callSheetData = () => {
     setLoadingSpread(true);
@@ -130,89 +86,17 @@ export default function DataCalculation() {
           gap={2}
           flexWrap="wrap"
         >
-          <Autocomplete
-            disablePortal
-            id="q1"
-            options={loading ? [] : q1Data}
-            value={selectedQ1Data}
-            onChange={(e, v) => setSelectedQ1Data(v)}
-            sx={{ width: "100%", maxWidth: 250, flexGrow: 1 }}
-            getOptionLabel={(q1Data) => q1Data.Item || ""}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label={
-                  loading ? (
-                    <CircularProgress size={24} color="primary" />
-                  ) : (
-                    "I am from"
-                  )
-                }
-                size="small"
-              />
-            )}
-          />
-          <Autocomplete
-            disablePortal
-            id="q2"
-            options={loading ? [] : q2Data}
-            value={selectedQ2Data}
-            onChange={(e, v) => setSelectedQ2Data(v)}
-            sx={{ width: "100%", maxWidth: 250, flexGrow: 1 }}
-            getOptionLabel={(q2Data) => q2Data.Item || ""}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label={
-                  loading ? (
-                    <CircularProgress size={24} color="primary" />
-                  ) : (
-                    "How to"
-                  )
-                }
-                size="small"
-              />
-            )}
-          />
-          <Autocomplete
-            disablePortal
-            id="q3"
-            options={loading ? [] : q3Data}
-            value={selectedQ3Data}
-            onChange={(e, v) => setSelectedQ3Data(v)}
-            sx={{ width: "100%", maxWidth: 250, flexGrow: 1 }}
-            getOptionLabel={(q3Data) => q3Data.Item || ""}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                label={
-                  loading ? (
-                    <CircularProgress size={24} color="primary" />
-                  ) : (
-                    "For"
-                  )
-                }
-                size="small"
-              />
-            )}
-          />
-
           <LoadingButton
             loading={loadingSpread}
             variant="contained"
             color="info"
             onClick={(event) => {
               callSheetData();
-              handleFormSubmit(event);
             }}
             disabled={isSubmitDisabled}
             sx={{ width: "100%", maxWidth: 250 }}
           >
-            {loading ? (
-              <CircularProgress size={24} color="primary" />
-            ) : (
-              "Calculate"
-            )}
+            Calculate
           </LoadingButton>
         </Box>
 
