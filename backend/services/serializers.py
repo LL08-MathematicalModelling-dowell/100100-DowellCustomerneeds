8a656cac04c17d5168ce2c8a4214da6d5a63b584
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-07-18 11:23:11 +0600

    propeName Define complete

--- backend/services/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Services
-
-
-class ServicesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Services
-        fields = '__all__'
--- backend/services/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ServiceView
-
-urlpatterns = [
-    path('api/services/', ServiceView.as_view())
-]
